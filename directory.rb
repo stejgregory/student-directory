e4a8046a4e6d3f5e2cb20b85a7d7ec9b39676335
Stephen Gregory <stejgregory@gmail.com>
2016-09-21 00:32:18 +0100

    An array of code for the 1st time

--- directory.rb
+++ directory.rb
@@ -1,17 +1,23 @@
-student_count = 11
-# First we print the list of our students
+
+# Let's put all the students into an [array]
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
+# and then print them
 puts "The students of Villains Academy"
 puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-# Finally, we print the final number of students
-print "Overall, we have #{student_count} great students"
+students.each do |student|      # For each iteration of the array 'students'
+  puts student                  # it is assigned to the variable |student|
+end                             # and the code between do-end is executed.
+# Finally, we print the total
+puts "Overall, we have #{students.count} great students"
