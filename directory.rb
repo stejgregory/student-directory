672f51b2eba9f46d37302029512221cdc58183ae
Stephen Gregory <stejgregory@gmail.com>
2016-09-21 02:16:09 +0100

    Hashing it up with CURLY brackets

--- directory.rb
+++ directory.rb
@@ -1,17 +1,17 @@
 
 # Let's put all the students into an [array]
-students = [                              # This is still a variable
-  "Dr. Hannibal Lecter",                  # even though it's an array
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
+students = [                                    # This is still a variable
+  {name: "Dr. Hannibal Lecter", cohort: :november},           # even though it's an array
+  {name: "Darth Vader", cohort: :november},                   # now arrays within an array
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
 ]
 
 def print_header
@@ -19,10 +19,10 @@ puts "The students of Villains Academy"
 puts "-------------"
 end
 
-def print(names)                  # We can now call this method and pass in 'students' array
-  names.each do |name|            # For each iteration of the array 'students'
-    puts name                     # it is assigned to the variable |student|
-  end                             # and the code between do-end is executed.
+def print(students)                  # We can now call this method and pass in 'students' array
+  students.each do |student|         # For each iteration of the array 'students'
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"     # it is assigned to the variable |student|
+  end                                               # and the code between do-end is executed.
 end   #also end method
 
 def print_footer(names)
